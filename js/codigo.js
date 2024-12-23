d488e65cfd0eadc040992a9d8405ba14c270afef
assolucionesmultimediales <multimediales.solutions@gmail.com>
2024-12-22 21:24:12 -0300

    first commit

---
+++ js/codigo.js
@@ -0,0 +1,8 @@
+let irAfiltros = document.getElementById("ir-a-filtros");
+let divFiltros = document.getElementById("contenedor-filtros");
+let divDiscos = document.getElementById("contenedor-discos");
+
+function filtros() {
+    divDiscos.style.display = "none";
+    divFiltros.style.display = "flex";
+}
